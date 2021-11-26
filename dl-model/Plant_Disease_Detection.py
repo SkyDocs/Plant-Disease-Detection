447e72a89c27f556f695927eef38cfce9c640858
Harshit Ruwali <harshitruwali@outlook.com>
2021-11-26 20:56:43 +0530

    nn is converging

--- dl-model/Plant_Disease_Detection.py
+++ dl-model/Plant_Disease_Detection.py
@@ -1,60 +1,66 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# # Github repo url
-# 
-# [https://github.com/SkyDocs/Plant-Disease-Detection](https://github.com/SkyDocs/Plant-Disease-Detection)
-
-# In[1]:
+# In[ ]:
 
 
-import os
+# get_ipython().system('wget https://github.com/nandakishormpai2001/Plant_Disease_Detector/raw/main/model/dataset.zip')
+
+
+# In[ ]:
+
+
+# get_ipython().system('unzip dataset.zip')
+
+
+# In[ ]:
+
+
+# get_ipython().system('pip install -r requirements.txt')
+
+
+# In[27]:
+
 
 from PIL import Image
 import torch
-import torch.nn as nn
-import torch.nn.functional as F
-import torchvision.transforms as transforms
+import torchvision
 from torchvision.transforms import ToTensor
+import torchvision.transforms as transforms
+import numpy as np
+import matplotlib.pyplot as plt
+
+import torch.nn as nn
 import torch.optim as optim
+import torch.nn.functional as F
+import os
+import pickle
 
 
-# In[2]:
-
-
-torch.cuda.is_available()
-
-
-# In[3]:
-
-
-#!unzip dataset.zip
-
-
-# dataset classification
-
-# In[4]:
+# In[28]:
 
 
 class trainData():
-    
     def __init__(self):
         self.labels,self.images = self.load_data()
 
+    # To load images and labels for dataloader
     def load_data(self):
-        labels = {}
+        labels={}
         images = {}
         count = 0
-        
+        # setting resize dimensions
         resize = transforms.Compose([transforms.Resize((256,256))])
         main_dir = os.listdir(os.path.join("dataset","train"))
         reference = {}
-        
+        # iterating through categories
         for i,dir in enumerate(main_dir):
             reference[dir]=i
             images_list = os.listdir(os.path.join("dataset","train",dir))
             local_cnt = 0
+            # iterating through images in a category
             for img in images_list:
+                # 500 images from each category
                 if local_cnt<500:
                     labels[count] = i
                     img_path = os.path.join("dataset","train",dir,img)
@@ -68,173 +74,157 @@ class trainData():
 
         print(reference)
         return labels,images
-    
-    
-#     def load_data(self):
-#         resize = transforms.Compose([transforms.Resize((256, 256))])
-#         train_dir = os.listdir(os.path.join("dataset", "train"))
-        
-#         ref = {}
-#         labels = {}
-#         image_list = {}
-#         global_count = 0
-         
-#         for i,dir in enumerate(train_dir):
-#             ref[dir] = i
-#             images = os.listdir(os.path.join("dataset", "train", dir))
-#             count = 0
-#             for img in images:
-#                 if count < 500:
-#                     labels[global_count] = i
-#                     img = os.path.join("dataset", "train", dir, img)
-#                     image = Image.open(img)
-#                     image = ToTensor()(image)
-#                     image_list[count] = resize(image)
-#                     count += 1
-#                     global_count += 1
-#                 else:
-#                     pirnt("Taken 500 images for training")
-#                     break
-        
-#         print(ref)
-#         return labels,image_list
-    
+      
     def __len__(self):
         return len(self.labels)
     
+
     # To return x,y values in each iteration over dataloader as batches.
     def __getitem__(self, idx):
-        return(self.images[idx], self.labels[idx])
+        return (
+            self.images[idx],
+            self.labels[idx],
+        )
 
 
-# In[5]:
+# In[29]:
 
 
 train_data = trainData()
 
 
-# In[6]:
+# In[30]:
 
 
+# Inherit from Dataset class
 class validationData(trainData):
-    
+
     def load_data(self):
-        resize = transforms.Compose([transforms.Resize((256,256))])
-        valid_dir = os.listdir(os.path.join("dataset", "valid"))
-        
-        labels={}
-        images = {}
-        global_count = 0
-         
-        for i,dir in enumerate(valid_dir):
-            print(i,dir)
-            image_path = os.listdir(os.path.join("dataset", "valid", dir))
-            count = 0
-            for img in image_path:
-                if(count<100):
-                    labels[global_count] = i
-                    img = os.path.join("dataset", "valid", dir, img)
-                    image = Image.open(img)
-                    image = ToTensor()(image)
-                    images[count] = resize(image)
-                    global_count+=1
-                    count+=1
-                else:
-                    break
-                    
-        return labels,images
+          labels={}
+          images = {}
+          count = 0
+          resize = transforms.Compose([transforms.Resize((256,256))])
+          main_dir = os.listdir(os.path.join("dataset","valid"))
+          for i,dir in enumerate(main_dir):
+              print(i,dir)
+              images_list = os.listdir(os.path.join("dataset","valid",dir))
+              local_cnt = 0
+              for img in images_list:
+                  if(local_cnt<100):
+                      labels[count] = i
+                      img_path = os.path.join("dataset","valid",dir,img)
+                      image = Image.open(img_path)
+                      image = ToTensor()(image)
+                      images[count] = resize(image)
+                      count+=1
+                      local_cnt+=1
+                  else:
+                      break
+
+          return labels,images
 
 
-# In[7]:
+# In[31]:
 
 
-valid_data = validationData()
-
-
-# In[8]:
-
-
-def get_num_correct(preds, labels):
-    return preds.argmax(dim=1).eq(labels).sum().item()
-
-
-# model
-
-# In[9]:
-
-
+# Model Architecture
 class neuralNet(nn.Module):
     def __init__(self):
-        super(neuralNet, self).__init__()
+        super(neuralNet,self).__init__()
+
+        # CNNs for rgb images 
+        self.conv1= nn.Conv2d(in_channels=3,out_channels=6,kernel_size=5)
+        self.conv2= nn.Conv2d(in_channels=6,out_channels=12,kernel_size=5)
+        self.conv3= nn.Conv2d(in_channels=12,out_channels=24,kernel_size=5)
+        self.conv4= nn.Conv2d(in_channels=24,out_channels=48,kernel_size=5)
         
-        self.conv1 = nn.Conv2d(in_channels=3, out_channels=6, kernel_size=5)
-        self.conv2 = nn.Conv2d(in_channels=6, out_channels=12, kernel_size=5)
-        self.conv3 = nn.Conv2d(in_channels=12, out_channels=24, kernel_size=5)
-        self.conv4 = nn.Conv2d(in_channels=24, out_channels=48, kernel_size=5)
-        
+        # Connecting CNN outputs with Fully Connected layers
         self.fc1 = nn.Linear(in_features=48*12*12,out_features=240)
         self.fc2 = nn.Linear(in_features=240,out_features=120)
         self.out = nn.Linear(in_features=120,out_features=17)
         
-    def forward(self, x):
-        x = x
         
-        x = self.conv1(x)
-        x = F.relu(x)
-        x = F.max_pool2d(x, kernel_size=2, stride=2)
+    def forward(self,t):
+        t = t
         
-        x = self.conv2(x)
-        x = F.relu(x)
-        x = F.max_pool2d(x, kernel_size=2, stride=2)
+        t=self.conv1(t)
+        t=F.relu(t)
+        t=F.max_pool2d(t,kernel_size = 2, stride = 2)
         
-        x = self.conv3(x)
-        x = F.relu(x)
-        x = F.max_pool2d(x, kernel_size=2, stride=2)
         
-        x = self.conv4(x)
-        x = F.relu(x)
-        x = F.max_pool2d(x, kernel_size=2, stride=2)
+        t=self.conv2(t)
+        t=F.relu(t)
+        t=F.max_pool2d(t,kernel_size = 2, stride = 2)
+
+        t=self.conv3(t)
+        t=F.relu(t)
+        t=F.max_pool2d(t,kernel_size = 2, stride = 2)
+
+        t=self.conv4(t)
+        t=F.relu(t)
+        t=F.max_pool2d(t,kernel_size = 2, stride = 2)
         
-        x = x.reshape(-1, 48*12*12)
-        x = self.fc1(x)
-        x = F.relu(x)
+        t=t.reshape(-1,48*12*12)
+        t=self.fc1(t)
+        t=F.relu(t)
         
-        x = self.fc2(x)
-        x = F.relu(x)
         
-        x = self.out(x)
+        t=self.fc2(t)
+        t=F.relu(t)
         
-        return x
+        t=self.out(t)
+        
+        
+        return t
 
 
-# In[10]:
+# In[32]:
 
 
 model = neuralNet()
 
 
-# training
-
-# In[11]:
+# In[33]:
 
 
-def train(train_data, valid_data, model):
+# dataset = Dataset()
+
+
+# In[34]:
+
+
+# valdataset = ValDataset()
+valid_data = validationData()
+
+# In[35]:
+
+
+# Function to return number of correct predictions in a batch
+def get_num_correct(preds,labels):
+    return preds.argmax(dim=1).eq(labels).sum().item()
+
+
+# In[36]:
+
+
+def train(train_data,valid_data, model):
     model.train()
-    
-    trainData = torch.utils.data.DataLoader(train_data, batch_size=64,shuffle=True)
-    validData = torch.utils.data.DataLoader(valid_data, batch_size=32,shuffle=True)
-    
-    
+
+    # dataloader in pytorch to load validation and train dataset
+    dataloader = torch.utils.data.DataLoader(train_data, batch_size=64,shuffle=True)
+    valdataloader = torch.utils.data.DataLoader(valid_data, batch_size=32,shuffle=True)
+
+    # Defining the loss and optimizer
     criterion = nn.CrossEntropyLoss()
     optimizer = optim.Adam(model.parameters(), lr=0.001)
-    num_of_epochs = 5
+    num_of_epochs = 20
     epochs = []
     losses = []
     for epoch in range(num_of_epochs):
         cnt = 0
         tot_loss = 0
         tot_correct = 0
-        for batch, (x, y) in enumerate(trainData):
+        for batch, (x, y) in enumerate(dataloader):
             # Sets the gradients of all optimized tensors to zero
             optimizer.zero_grad()
             y_pred = model(x)
@@ -244,7 +234,7 @@ def train(train_data, valid_data, model):
             loss.backward()
             optimizer.step()
 
-        for batch, (x, y) in enumerate(validData):
+        for batch, (x, y) in enumerate(valdataloader):
             # Sets the gradients of all optimized tensors to zero
             optimizer.zero_grad()
             with torch.no_grad():
@@ -252,28 +242,57 @@ def train(train_data, valid_data, model):
                 # Compute loss (here CrossEntropyLoss)
                 loss = F.cross_entropy(y_pred,y)
 
-            tot_loss += loss.item()
-            tot_correct += get_num_correct(y_pred,y)
+            tot_loss+=loss.item()
+            tot_correct +=get_num_correct(y_pred,y)
         epochs.append(epoch)
         losses.append(tot_loss)
         print("Epoch",epoch,"total_correct",tot_correct,"loss:",tot_loss)
         torch.save(model.state_dict(), "model002_ep"+str(epoch+1)+".pth")
 
-
-# In[12]:
-
-
-train(train_data, valid_data, model)
+# In[37]:
 
 
-# In[ ]:
-
-
-
+train(train_data,valid_data, model)
 
 
 # In[ ]:
 
 
+# Saving labels to label value as a json
+main_dir = os.listdir(os.path.join("minidataset","train"))
+reference = {}
+for i,dir in enumerate(main_dir):
+    reference[dir]=i
+with open('labels.json', 'wb') as iw:
+    pickle.dump(reference, iw)
 
+
+# In[ ]:
+
+
+#Save the trained model
+torch.save(model.state_dict(), "model.pth")
+
+
+# In[ ]:
+
+
+# prediction function to test
+def predict(img_path):
+    image = Image.open(img_path)
+    image = ToTensor()(image)
+    resize = transforms.Compose([transforms.Resize((256,256))])
+    y_result = model(resize(image).unsqueeze(0))
+    result_idx = y_result.argmax(dim=1)
+    for key,value in reference.items():
+        if(value==result_idx):
+            print(key)
+            break
+      
+
+
+# In[ ]:
+
+
+predict("img_path")
 

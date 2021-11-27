bf94c5df207ea292df4f1b620dc62d7f9ef52bd0
Harshit Ruwali <harshitruwali@outlook.com>
2021-11-27 17:36:56 +0530

    working on the api-backend

---
+++ api-backend/main.py
@@ -0,0 +1,29 @@
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+
+import base64
+
+app = Flask("Plant Disease Detection")
+
+CORS(app)
+
+@app.route('/', methods=['POST'])
+
+def predict():
+	data_ret = request.get_json()
+	image = data_ret["image"]
+	image = base64.b64decode(image)
+	#predict
+	model = neuralNetwork()
+	result = predict(model, image)
+
+	response = {
+		result
+	}
+
+	response = jsonify(result)
+	return response
+
+
+if __name__ == '__main__':
+	app.run(host='127.0.0.1', port=8080)

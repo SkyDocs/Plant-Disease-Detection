6f93d03eb715c8b1838f1d0bad622b6a6b7a5dab
Harshit Ruwali <harshitruwali@outlook.com>
2022-01-20 23:14:23 +0530

    ported the flask app to tensorflow

--- api-backend/src/model/dl_predict.py
+++ api-backend/src/model/dl_predict.py
@@ -1,62 +1,49 @@
 import io
-import torch
-import torch.nn as nn
-import torch.nn.functional as F
-from torchvision.transforms import ToTensor
-import torchvision.transforms as transforms
-
-import pickle
 from PIL import Image
+import numpy as np
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing import image
 
-class neuralNet(nn.Module):
-	def __init__(self):
-		super(neuralNet,self).__init__()
+class_names = ['Apple___Apple_scab',
+ 'Apple___Black_rot',
+ 'Apple___Cedar_apple_rust',
+ 'Apple___healthy',
+ 'Blueberry___healthy',
+ 'Cherry_(including_sour)___Powdery_mildew',
+ 'Cherry_(including_sour)___healthy',
+ 'Corn_(maize)___Cercospora_leaf_spot Gray_leaf_spot',
+ 'Corn_(maize)___Common_rust_',
+ 'Corn_(maize)___Northern_Leaf_Blight',
+ 'Corn_(maize)___healthy',
+ 'Grape___Black_rot',
+ 'Grape___Esca_(Black_Measles)',
+ 'Grape___Leaf_blight_(Isariopsis_Leaf_Spot)',
+ 'Grape___healthy',
+ 'Orange___Haunglongbing_(Citrus_greening)',
+ 'Peach___Bacterial_spot',
+ 'Peach___healthy',
+ 'Pepper,_bell___Bacterial_spot',
+ 'Pepper,_bell___healthy',
+ 'Potato___Early_blight',
+ 'Potato___Late_blight',
+ 'Potato___healthy',
+ 'Raspberry___healthy',
+ 'Soybean___healthy',
+ 'Squash___Powdery_mildew',
+ 'Strawberry___Leaf_scorch',
+ 'Strawberry___healthy',
+ 'Tomato___Bacterial_spot',
+ 'Tomato___Early_blight',
+ 'Tomato___Late_blight',
+ 'Tomato___Leaf_Mold',
+ 'Tomato___Septoria_leaf_spot',
+ 'Tomato___Spider_mites Two-spotted_spider_mite',
+ 'Tomato___Target_Spot',
+ 'Tomato___Tomato_Yellow_Leaf_Curl_Virus',
+ 'Tomato___Tomato_mosaic_virus',
+ 'Tomato___healthy']
 
-		self.conv1= nn.Conv2d(in_channels=3,out_channels=6,kernel_size=5)
-		self.conv2= nn.Conv2d(in_channels=6,out_channels=12,kernel_size=5)
-		self.conv3= nn.Conv2d(in_channels=12,out_channels=24,kernel_size=5)
-		self.conv4= nn.Conv2d(in_channels=24,out_channels=48,kernel_size=5)
-		
-		self.fc1 = nn.Linear(in_features=48*12*12,out_features=240)
-		self.fc2 = nn.Linear(in_features=240,out_features=120)
-		self.out = nn.Linear(in_features=120,out_features=17)
-		
-		
-	def forward(self,t):
-		t = t
-		
-		t=self.conv1(t)
-		t=F.relu(t)
-		t=F.max_pool2d(t,kernel_size = 2, stride = 2)
-		
-		
-		t=self.conv2(t)
-		t=F.relu(t)
-		t=F.max_pool2d(t,kernel_size = 2, stride = 2)
-
-		t=self.conv3(t)
-		t=F.relu(t)
-		t=F.max_pool2d(t,kernel_size = 2, stride = 2)
-
-		t=self.conv4(t)
-		t=F.relu(t)
-		t=F.max_pool2d(t,kernel_size = 2, stride = 2)
-		
-		t=t.reshape(-1,48*12*12)
-		t=self.fc1(t)
-		t=F.relu(t)
-		
-		
-		t=self.fc2(t)
-		t=F.relu(t)
-		
-		t=self.out(t)
-		
-		
-		return t
-
-
-
+model = load_model('model/model.h5')
 
 def get_remedy(plant_disease):
 	with open("model/disease.json", 'r') as f:
@@ -67,33 +54,35 @@ def get_remedy(plant_disease):
 			return(remedies[key])
 
 
-def predict_disease(model, image):
-	with open('model/labels.json', 'rb') as labels:
-		labels = pickle.load(labels)
+def predict_disease(image):
+	img = Image.open(io.BytesIO(image))
+	img = img.convert('RGB')
+	img = img.resize((256, 256), Image.NEAREST)
 
-	model = model
-	model.load_state_dict(torch.load("model/model.pth"))
-	model.eval
+	# img = image.load_img(image, target_size=(256, 256))
+	#x = image.img_to_array(img)
+	x = np.asarray(img)
+	x = np.expand_dims(x, axis=0)
+	pred = model.predict(x)
 
-	image = Image.open(io.BytesIO(image))
-	resize = transforms.Compose([transforms.Resize((256, 256))])
-	image = ToTensor()(image)
-
-	y_result = model(resize(image).unsqueeze(0))
-	result_idx = y_result.argmax(dim=1)
-
-	for key,value in labels.items():
-		if(value == result_idx):
-			disease = key
-			break
+	# print("prediction", pred)
 	
-	if disease != "healthy":
+	d = pred.flatten()
+	j = d.max()
+	for index,item in enumerate(d):
+		if item == j:
+			disease = class_names[index]
+
+	# confidence = round(100 * j, 3)
+	# print(disease, confidence)
+
+	if "healthy" not in disease:
 		try:
 			remedy = get_remedy(disease)
 		except:
 			remedy = "Sorry the remedy is not available"
-		else:
-			remedy = "NAN, the plant is healthy"
-
-	return disease, remedy
+	else:
+		remedy = "NAN, the plant is healthy"
+	
+	return disease, remedy 
 

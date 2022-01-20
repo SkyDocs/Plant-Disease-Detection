85a56a735fbcc3bdc079b94340f04ab0a871b5b1
Harshit Ruwali <harshitruwali@outlook.com>
2022-01-20 20:18:56 +0530

    fine tuning neural net

--- api-backend/src/model/dl_predict.py
+++ api-backend/src/model/dl_predict.py
@@ -9,62 +9,62 @@ import pickle
 from PIL import Image
 
 class neuralNet(nn.Module):
-    def __init__(self):
-        super(neuralNet,self).__init__()
+	def __init__(self):
+		super(neuralNet,self).__init__()
 
-        self.conv1= nn.Conv2d(in_channels=3,out_channels=6,kernel_size=5)
-        self.conv2= nn.Conv2d(in_channels=6,out_channels=12,kernel_size=5)
-        self.conv3= nn.Conv2d(in_channels=12,out_channels=24,kernel_size=5)
-        self.conv4= nn.Conv2d(in_channels=24,out_channels=48,kernel_size=5)
-        
-        self.fc1 = nn.Linear(in_features=48*12*12,out_features=240)
-        self.fc2 = nn.Linear(in_features=240,out_features=120)
-        self.out = nn.Linear(in_features=120,out_features=17)
-        
-        
-    def forward(self,t):
-        t = t
-        
-        t=self.conv1(t)
-        t=F.relu(t)
-        t=F.max_pool2d(t,kernel_size = 2, stride = 2)
-        
-        
-        t=self.conv2(t)
-        t=F.relu(t)
-        t=F.max_pool2d(t,kernel_size = 2, stride = 2)
+		self.conv1= nn.Conv2d(in_channels=3,out_channels=6,kernel_size=5)
+		self.conv2= nn.Conv2d(in_channels=6,out_channels=12,kernel_size=5)
+		self.conv3= nn.Conv2d(in_channels=12,out_channels=24,kernel_size=5)
+		self.conv4= nn.Conv2d(in_channels=24,out_channels=48,kernel_size=5)
+		
+		self.fc1 = nn.Linear(in_features=48*12*12,out_features=240)
+		self.fc2 = nn.Linear(in_features=240,out_features=120)
+		self.out = nn.Linear(in_features=120,out_features=17)
+		
+		
+	def forward(self,t):
+		t = t
+		
+		t=self.conv1(t)
+		t=F.relu(t)
+		t=F.max_pool2d(t,kernel_size = 2, stride = 2)
+		
+		
+		t=self.conv2(t)
+		t=F.relu(t)
+		t=F.max_pool2d(t,kernel_size = 2, stride = 2)
 
-        t=self.conv3(t)
-        t=F.relu(t)
-        t=F.max_pool2d(t,kernel_size = 2, stride = 2)
+		t=self.conv3(t)
+		t=F.relu(t)
+		t=F.max_pool2d(t,kernel_size = 2, stride = 2)
 
-        t=self.conv4(t)
-        t=F.relu(t)
-        t=F.max_pool2d(t,kernel_size = 2, stride = 2)
-        
-        t=t.reshape(-1,48*12*12)
-        t=self.fc1(t)
-        t=F.relu(t)
-        
-        
-        t=self.fc2(t)
-        t=F.relu(t)
-        
-        t=self.out(t)
-        
-        
-        return t
+		t=self.conv4(t)
+		t=F.relu(t)
+		t=F.max_pool2d(t,kernel_size = 2, stride = 2)
+		
+		t=t.reshape(-1,48*12*12)
+		t=self.fc1(t)
+		t=F.relu(t)
+		
+		
+		t=self.fc2(t)
+		t=F.relu(t)
+		
+		t=self.out(t)
+		
+		
+		return t
 
 
 
 
 def get_remedy(plant_disease):
-    with open("model/disease.json", 'r') as f:
-        remedies = json.load(f)
+	with open("model/disease.json", 'r') as f:
+		remedies = json.load(f)
 
-    for key in remedies:
-        if key == plant_disease:
-            return(remedies[key])
+	for key in remedies:
+		if key == plant_disease:
+			return(remedies[key])
 
 
 def predict_disease(model, image):
@@ -86,14 +86,14 @@ def predict_disease(model, image):
 		if(value == result_idx):
 			disease = key
 			break
-    
-    if disease != "healthy":
-        try:
-            remedy = get_remedy(disease)
-        except:
-            remedy = "Sorry the remedy is not available"
-    else:
-        remedy = "NAN, the plant is healthy"	
 	
-    return disease, remedy
+	if disease != "healthy":
+		try:
+			remedy = get_remedy(disease)
+		except:
+			remedy = "Sorry the remedy is not available"
+		else:
+			remedy = "NAN, the plant is healthy"
+
+	return disease, remedy
 

75ba795fe93e869ac7b1cf779d695012f88e72c4
Harshit Ruwali <harshitruwali@outlook.com>
2022-02-23 00:20:46 +0530

    model re-trained for less biased results

--- api-backend/src/model/dl_predict.py
+++ api-backend/src/model/dl_predict.py
@@ -6,24 +6,24 @@ import numpy as np
 from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing import image
 
-class_names = [
+class_names = ['Apple___Cedar_apple_rust',
+ 'Apple___healthy',
  'Cherry_(including_sour)___Powdery_mildew',
  'Cherry_(including_sour)___healthy',
+ 'Corn_(maize)___Northern_Leaf_Blight',
+ 'Corn_(maize)___healthy',
+ 'Grape___Black_rot',
+ 'Grape___healthy',
+ 'Peach___Bacterial_spot',
+ 'Peach___healthy',
+ 'Pepper,_bell___Bacterial_spot',
  'Pepper,_bell___healthy',
- 'Potato___Early_blight',
  'Potato___Late_blight',
  'Potato___healthy',
- 'Tomato___Bacterial_spot',
- 'Tomato___Early_blight',
- 'Tomato___Late_blight',
- 'Tomato___Leaf_Mold',
+ 'Strawberry___Leaf_scorch',
+ 'Strawberry___healthy',
  'Tomato___Septoria_leaf_spot',
- 'Tomato___Spider_mites Two-spotted_spider_mite',
- 'Tomato___Target_Spot',
- 'Tomato___Tomato_Yellow_Leaf_Curl_Virus',
- 'Tomato___Tomato_mosaic_virus',
- 'Tomato___healthy'
- ]
+ 'Tomato___healthy']
 
 model = load_model('model/model.h5')
 
